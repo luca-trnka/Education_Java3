e59abd2af96d4e0bed3500b4647028a10e3689b8
luca-trnka <lucie-trnkova76@seznam.cz>
2024-05-22 15:34:44 +0200

    Other tests for gameTask class done: generateGameByGenre, eneratePublishersGamesCount

--- src/test/java/org/example/GameTaskTest.java
+++ src/test/java/org/example/GameTaskTest.java
@@ -36,17 +36,20 @@ class GameTaskTest {
         lenient().when(fileManipulator.readLinesFromFile()).thenReturn(Arrays.asList(
                 "titles,released,developers,publishers,genres",
                 "Game1,2020,Dev1,Pub1,\"Action, Adventure\"",
-                "Game2,2019,Dev2,Pub2,\"Adventure, Puzzle\""
+                "Game2,2019,Dev2,Pub2,\"Adventure, Puzzle\"",
+                "Game3,2019,Dev3,Pub2,Arcade"
         ));
         List<Map<String, String>> csvData = List.of(
                 Map.of("titles", "Game1", "released", "2020", "developers", "Dev1", "publishers", "Pub1", "genres", "Action, Adventure"),
-                Map.of("titles", "Game2", "released", "2019", "developers", "Dev2", "publishers", "Pub2", "genres", "Adventure, Puzzle")
+                Map.of("titles", "Game2", "released", "2019", "developers", "Dev2", "publishers", "Pub2", "genres", "Adventure, Puzzle"),
+                Map.of("titles", "Game3", "released", "2019", "developers", "Dev3", "publishers", "Pub2", "genres", "Arcade")
         );
         when(csvManipulator.getData()).thenReturn(csvData);
 
         List<Game> games = List.of(
                 new Game("Game1", 2020, Arrays.asList("Dev1"), Arrays.asList("Pub1"), Arrays.asList("Action", "Adventure")),
-                new Game("Game2", 2019, Arrays.asList("Dev2"), Arrays.asList("Pub2"), Arrays.asList("Adventure", "Puzzle"))
+                new Game("Game2", 2019, Arrays.asList("Dev2"), Arrays.asList("Pub2"), Arrays.asList("Adventure", "Puzzle")),
+                new Game("Game3", 2019, Arrays.asList("Dev3"), Arrays.asList("Pub2"), Arrays.asList("Arcade"))
         );
         when(gameConvertor.gameExtractionFromData(csvData)).thenReturn(games);
 
@@ -71,10 +74,53 @@ class GameTaskTest {
 
         assertEquals(outputFilePath, filePathCaptor.getValue());
 
-        String expectedContent = "Action,Adventure,Puzzle";
+        String expectedContent = "Action,Adventure,Arcade,Puzzle";
         assertEquals(expectedContent, contentCaptor.getValue());
 
-        System.out.println("File path: " + filePathCaptor.getValue());
+        System.out.println("File content: " + contentCaptor.getValue());
+    }
+
+    @Test
+    void testGenerateGamesByGenre() throws Exception {
+        String outputFilePath = "src/test/resources/test-adventure-games.csv";
+        String wantedGenre = "Adventure";
+
+        gameTask.generateGamesByGenre(outputFilePath, wantedGenre);
+
+        ArgumentCaptor<String> filePathCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> contentCaptor = ArgumentCaptor.forClass(String.class);
+
+        verify(fileManipulator).writeTextToFile(filePathCaptor.capture(), contentCaptor.capture());
+
+        assertEquals(outputFilePath, filePathCaptor.getValue());
+
+        String expectedContent = "Title, Year" + System.lineSeparator() +
+                "Game2, 2019" + System.lineSeparator() +
+                "Game1, 2020";
+        assertEquals(expectedContent, contentCaptor.getValue());
+
+        System.out.println("File content: " + contentCaptor.getValue());
+    }
+
+    @Test
+    void testGeneratePublishersGamesCount() throws Exception {
+        String outputFilePath = "src/test/resources/test-publishers-count.csv";
+
+        gameTask.generatePublishersGamesCount(outputFilePath);
+
+        ArgumentCaptor<String> filePathCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> contentCaptor = ArgumentCaptor.forClass(String.class);
+
+        verify(fileManipulator).writeTextToFile(filePathCaptor.capture(), contentCaptor.capture());
+
+        assertEquals(outputFilePath, filePathCaptor.getValue());
+
+        String expectedContent = "Publisher, Count" + System.lineSeparator() +
+                "Pub2, 2" + System.lineSeparator() +
+                "Pub1, 1";
+
+        assertEquals(expectedContent, contentCaptor.getValue());
+
         System.out.println("File content: " + contentCaptor.getValue());
     }
 }
